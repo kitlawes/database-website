be4edd665c9c5c282f3f8c969c488b4178b47978
Kit Lawes <kitlawes@hotmail.com>
2017-03-18 20:02:34 +0000

    Support retrieving titles from the database

--- src/index.php
+++ src/index.php
@@ -14,6 +14,12 @@
     <input type="submit">
 </form>
 
+<form action="<?php echo $_SERVER["PHP_SELF"];?>" method="post">
+    Titles
+    <input style="display: none;" name="query_type" value="titles">
+    <input type="submit">
+</form>
+
 <br />
 
 <?php
@@ -32,28 +38,30 @@ if ($_SERVER["REQUEST_METHOD"] == "POST") {
     
     $query_type = $_POST["query_type"];
     if ($query_type == "dept_names") {
-    
         $query = "SELECT dept_name FROM departments;";
-        $result = mysqli_query($connection, $query);
-      
-        $fields = mysqli_fetch_fields($result);
-        echo "<table border=\"1\"><tr>";
+    }
+    if ($query_type == "titles") {
+        $query = "SELECT DISTINCT title FROM titles;";
+    }
+    
+    $result = mysqli_query($connection, $query);
+    
+    $fields = mysqli_fetch_fields($result);
+    echo "<table border=\"1\"><tr>";
+    foreach ($fields as $field) {
+        echo "<th>" . $field->name . "</th>";
+    }
+    echo "</tr>";
+    
+    while ($row = $result->fetch_assoc()) {
+        echo "<tr>";
         foreach ($fields as $field) {
-            echo "<th>" . $field->name . "</th>";
+            echo "<td>" . $row[$field->name] . "</td>";
         }
         echo "</tr>";
-        
-        while ($row = $result->fetch_assoc()) {
-            echo "<tr>";
-            foreach ($fields as $field) {
-                echo "<td>" . $row[$field->name] . "</td>";
-            }
-            echo "</tr>";
-        }
-        
-        echo "</table>";
-    
     }
+    
+    echo "</table>";
         
     mysqli_close($connection);
 
